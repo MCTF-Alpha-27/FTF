1ae951f1ff7a25f4391bcfc40f1876f33de075d2
MCTF-Alpha-27 <j13816180940@139.com>
2023-02-09 18:01:43 +0800

    增加了远程关机功能，同时修复了微信反馈信息不完整的问题

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ log("正在连接到微信", "info")
 say_in_english("starting terminal")
 say_in_english("connecting")
 
+pyautogui.hotkey("ctrl", "alt", "w")
 app = Application(backend="uia").connect(process=get_wechat_pid())
 wechat_window = app.window(class_name="WeChatMainWndForPC")
-pyautogui.hotkey("ctrl", "alt", "w")
+wechat_window.minimize()
 pyautogui.FAILSAFE = False
 
 log("已尝试连接，如没有成功，请手动启动微信", "info")
@@ -21,7 +22,7 @@ try:
     say_in_english("start listening")
     executant_window = wechat_window.child_window(title="一只叫迷迭香的菲林", control_type="ListItem")
     executant_wrapper_object = executant_window.wrapper_object()
-    command_list = ["/start protocol", "/stop protocol"]
+    command_list = ["/start protocol", "/stop protocol", "/shutdown"]
     while True:
         for i in executant_wrapper_object.descendants():
             if i.friendly_class_name() == "Static":
@@ -33,16 +34,31 @@ try:
                         say_in_english("protocol activation command detected")
                         log("检测到协议激活指令", "info")
                         log("协议已激活", "info")
+                        pyautogui.hotkey("ctrl", "alt", "w")
                         executant_wrapper_object.click_input()
                         executant_wrapper_object.type_keys("[FTF] 协议已激活", with_spaces=True)
                         pyautogui.hotkey("enter")
+                        wechat_window.minimize()
                     elif command == "/stop protocol":
                         say_in_english("protocol terminated command detected")
                         log("检测到协议终止指令", "info")
                         log("协议已终止", "info")
+                        pyautogui.hotkey("ctrl", "alt", "w")
                         executant_wrapper_object.click_input()
                         executant_wrapper_object.type_keys("[FTF] 协议已终止", with_spaces=True)
                         pyautogui.hotkey("enter")
+                        wechat_window.minimize()
+                    elif command == "/shutdown":
+                        say_in_english("shutdown command detected")
+                        log("检测到关机指令", "info")
+                        say_in_english("the computer will shutdown in T-minus ten seconds")
+                        log("将在10秒后关机", "info")
+                        pyautogui.hotkey("ctrl", "alt", "w")
+                        executant_wrapper_object.click_input()
+                        executant_wrapper_object.type_keys("[FTF] 将在10秒后关机", with_spaces=True)
+                        pyautogui.hotkey("enter")
+                        wechat_window.minimize()
+                        os.system("shutdown -s -t 10")
         time.sleep(1)
 except Exception as e:
     log("监听中发生错误，我们获取了以下信息", "warning")
